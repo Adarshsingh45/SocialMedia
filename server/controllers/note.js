2a20c6498ec8989202631005fb215a1a981edd59
Adarsh Singh <adarshsingh452002@gmail.com>
2023-10-31 19:15:12 +0530

    Added addremove

--- server/controllers/note.js
+++ server/controllers/note.js
@@ -1,4 +1,5 @@
 import Note from '../models/Note.js';
+import Users from '../models/Users.js';
 
 export const getNotes = async (req,res)=>{
     try{
@@ -9,3 +10,20 @@ export const getNotes = async (req,res)=>{
         res.status(500).json({message: err.message});
     }
 };
+
+export const addRemoveNote = async (req,res)=>{
+    try{
+        const {id,noteId} = req.params
+        const user = await Users.findById(id);
+        const note = await Note.findById(noteId);
+        if (user.Note.includes(noteId)) {
+            user.Note = user.Note.filter((id) => id !== noteId);
+            note.Note = note.Note.filter((id) => id !== id);
+          } else {
+            user.Note.push(noteId);
+            note.Note.push(id);
+          }
+    }catch(err){
+        res.status(500).json({message: err.message});
+    }
+}
