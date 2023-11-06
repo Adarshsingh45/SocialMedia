e0a66b14f311d6cf81b82ecd875892529078ff00
Adarsh Singh <adarshsingh452002@gmail.com>
2023-11-06 08:22:17 +0530

    added state

---
+++ client/src/state/index.js
@@ -0,0 +1,33 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const initialState={
+    user:null,
+    token:null,
+    notes:[],
+};
+
+export const authSlice = createSlice({
+    name:'auth',
+    initialState,
+    reducers:{
+        setLogin:(state,action)=>{
+            state.user=action.payload.user;
+            state.token=action.payload.token;
+        },
+        setLogout:(state)=>{
+            state.user=null;
+            state.token=null;
+        },
+        setSubjects:(state,action)=>{
+            if(state.user){
+                state.user.subjects=action.payload.subjects;
+            }else{
+                console.error('user subjects not present');
+            }
+        },
+        setNotes:(state,action)=>{
+            state.notes=action.payload.notes;
+        },
+        
+    }
+})
